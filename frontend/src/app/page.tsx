10e417042c6e8001471ae7620d4191e11c9c284a
Peter Maliar <peter.maliar@tabsandspaces.agency>
2024-11-03 19:32:43 +0200

    Fixed 3

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="container mx-auto">
       <Suspense fallback={<p>Loading...</p>}>
-        <GetMessage />
+        {/*<GetMessage />*/}
       </Suspense>
     </main>
   );
